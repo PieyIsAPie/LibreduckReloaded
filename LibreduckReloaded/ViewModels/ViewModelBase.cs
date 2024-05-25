9f0c1c989fc94f95fa8674439c969f43b2afa87d
Jude Pearson-Phillips <foreynator.serverowner@gmail.com>
2024-05-25 13:16:38 -0400

    c# bad

--- LibreduckReloaded/ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using ReactiveUI;
-
-namespace LibreduckReloaded.ViewModels;
-
-public class ViewModelBase : ReactiveObject
-{
-}
---
+++ src/window.py
@@ -0,0 +1,8 @@
+from PySide6.QtWebEngineWidgets import *
+from PySide6.QtWidgets import *
+from PySide6.QtCore import *
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self)
+    
